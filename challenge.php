420a00a6f6bb0ef2df18085a24e1ac523fb85457
Reinier Sierag <reinier@kobaltdigital.nl>
2022-01-19 12:52:32 +0100

    Create challenge.php

---
+++ challenge.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ _   __      _           _ _     _____ _                     _____ _           _ _
+| | / /     | |         | | |   /  ___| |                   /  __ \ |         | | |
+| |/ /  ___ | |__   __ _| | |_  \ `--.| |_ __ _  __ _  ___  | /  \/ |__   __ _| | | ___ _ __   __ _  ___
+|    \ / _ \| '_ \ / _` | | __|  `--. \ __/ _` |/ _` |/ _ \ | |   | '_ \ / _` | | |/ _ \ '_ \ / _` |/ _ \
+| |\  \ (_) | |_) | (_| | | |_  /\__/ / || (_| | (_| |  __/ | \__/\ | | | (_| | | |  __/ | | | (_| |  __/
+\_| \_/\___/|_.__/ \__,_|_|\__| \____/ \__\__,_|\__, |\___|  \____/_| |_|\__,_|_|_|\___|_| |_|\__, |\___|
+                                                 __/ |                                         __/ |
+                                                |___/                                         |___/
+
+Hier heb je een lijst van alle mensen die bij Kobalt op kantoor te vinden zijn:
+Arne
+Reinier
+Kylian
+Huub
+Mick
+Brenda
+Ruben
+Desley
+Carly
+Esther
+
+De challenge:
+01: Sorteer de lijst.
+02: Bereken de alfabetische waarde van iedere naam (a=1, b=2, ..., z=26)
+03: Vermenigvuldig dit met de positie in de gesorteerde lijst. (eerste positie = x 1, tweeede positie = x 2)
+
+Voorbeeld:
+Appel
+Banaan
+
+Appel = 1 + 16 + 16 + 5 + 12 = 50
+Banaan = 2 + 1 + 14 + 1 + 1 + 14 = 33
+
+Totaalscore = 50 x 1 + 33 x 2 = 116
+
+De uitkomst:
+Wat is de totaal score?
+
+Voorwaarden:
+- Maak de challenge in Laravel, wij kijken naar de uitkomst, maar ook hoe je het met Laravel hebt opgezet.
+- Upload de code naar Github en stuur een link naar hello@kobaltdigital.nl
+
+ 
+*/
