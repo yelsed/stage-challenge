aec7b676d0cb35e0756395c533fff757fdd35e26
RBHoogeveen <43063842+RBHoogeveen@users.noreply.github.com>
2022-01-26 14:20:43 +0100

    Translate challenge.php to English.

--- challenge.php
+++ challenge.php
@@ -1,16 +1,15 @@
 <?php
 
 /*
- _   __      _           _ _     _____ _                     _____ _           _ _
-| | / /     | |         | | |   /  ___| |                   /  __ \ |         | | |
-| |/ /  ___ | |__   __ _| | |_  \ `--.| |_ __ _  __ _  ___  | /  \/ |__   __ _| | | ___ _ __   __ _  ___
-|    \ / _ \| '_ \ / _` | | __|  `--. \ __/ _` |/ _` |/ _ \ | |   | '_ \ / _` | | |/ _ \ '_ \ / _` |/ _ \
-| |\  \ (_) | |_) | (_| | | |_  /\__/ / || (_| | (_| |  __/ | \__/\ | | | (_| | | |  __/ | | | (_| |  __/
-\_| \_/\___/|_.__/ \__,_|_|\__| \____/ \__\__,_|\__, |\___|  \____/_| |_|\__,_|_|_|\___|_| |_|\__, |\___|
-                                                 __/ |                                         __/ |
-                                                |___/                                         |___/
-
-Hier heb je een lijst van alle mensen die bij Kobalt op kantoor te vinden zijn:
+  _  __     _           _ _     _____       _                      _     _          _____ _           _ _                       
+ | |/ /    | |         | | |   |_   _|     | |                    | |   (_)        / ____| |         | | |                      
+ | ' / ___ | |__   __ _| | |_    | |  _ __ | |_ ___ _ __ _ __  ___| |__  _ _ __   | |    | |__   __ _| | | ___ _ __   __ _  ___ 
+ |  < / _ \| '_ \ / _` | | __|   | | | '_ \| __/ _ \ '__| '_ \/ __| '_ \| | '_ \  | |    | '_ \ / _` | | |/ _ \ '_ \ / _` |/ _ \
+ | . \ (_) | |_) | (_| | | |_   _| |_| | | | ||  __/ |  | | | \__ \ | | | | |_) | | |____| | | | (_| | | |  __/ | | | (_| |  __/
+ |_|\_\___/|_.__/ \__,_|_|\__| |_____|_| |_|\__\___|_|  |_| |_|___/_| |_|_| .__/   \_____|_| |_|\__,_|_|_|\___|_| |_|\__, |\___|
+                                                                          | |                                         __/ |     
+                                                                          |_|                                        |___/      
+Here is a list of everyone you can find at the Kobalt office:
 Arne
 Reinier
 Kylian
@@ -22,31 +21,31 @@ Desley
 Carly
 Esther
 
-De challenge:
-01: Sorteer de lijst.
-02: Bereken de alfabetische waarde van iedere naam (a=1, b=2, ..., z=26)
-03: Vermenigvuldig dit met de positie in de gesorteerde lijst. (eerste positie = x 1, tweeede positie = x 2)
+The challenge:
+01: Sort the list.
+02: Calculate the alphabetic value of each name. (a=1, b=2, ..., z=26)
+03: Multiply this value with the position in the sorted list. (first position = x 1, second position = x 2)
 
-Voorbeeld:
-Appel
-Banaan
+Example:
+Apple
+Banana
 
-Appel = 1 + 16 + 16 + 5 + 12 = 50
-Banaan = 2 + 1 + 14 + 1 + 1 + 14 = 33
+Apple = 1 + 16 + 16 + 12 + 5 = 50
+Banana = 2 + 1 + 14 + 1 + 14 + 1 = 33
 
-Totaalscore = 50 x 1 + 33 x 2 = 116
+Total score = 50 x 1 + 33 x 2 = 116
 
-De uitkomst:
-Wat is de totaal score?
+The results:
+What is the total score?
 
-Voorwaarden:
-- Schat voor jezelf in hoeveel tijd dit gaat kosten.
-- Maak de challenge in Laravel, wij kijken naar de totaal score, maar ook hoe je het met Laravel hebt opgezet uiteraard.
-- Upload de code naar Github.
-- Stuur een link naar hello@kobaltdigital.nl samen met de daadwerkelijk bestede tijd en je bevindingen waarom je er langer over deed of je sneller klaar was.
+Requirements:
+- Make an estimate of the time you need to complete the challenge.
+- Make the challenge in Laravel. We will take a look at the total score, but also at the quality of your code.
+- Upload your code to GitHub.
+- Send a link to hello@kobaltdigital.nl with the actual time it took en why it took longer or shorter than your estimated time.
 
-Bonuspunten:
-- Commit regelmatig.
-- Vertel, wat vind je van deze challenge? #tipszijnwelkom
+Bonus points:
+- Commit frequently.
+- Tell us what you think of this challenge! #tipsarewelcome
 
 */
